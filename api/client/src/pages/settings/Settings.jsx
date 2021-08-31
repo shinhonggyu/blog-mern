1a366943f2e47850e68044484135b539ff02e6f8
shinhonggyu <zowoz0@naver.com>
2021-08-31 21:37:47 +0900

    update

--- api/client/src/pages/settings/Settings.jsx
+++ api/client/src/pages/settings/Settings.jsx
@@ -16,7 +16,7 @@ const Settings = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [success, setSuccess] = useState(false);
-  const PF = 'http://localhost:5000/images/';
+  const PF = 'https://lamadevblog.herokuapp.com/images/';
 
   const submitHandler = async (e) => {
     e.preventDefault();
